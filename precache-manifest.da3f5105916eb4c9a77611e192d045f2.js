31fbddd87a73896984b79b11f2b53d4b60a5a63b
rfkinline <robert@lightdec.com>
2020-09-23 15:29:10 -0400

    Updates

---
+++ precache-manifest.da3f5105916eb4c9a77611e192d045f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38b9170e48beae811ae8120a013001b6",
+    "url": "/rfkinline/IOT-React-coin-exchange/index.html"
+  },
+  {
+    "revision": "71db51d90cafd0153c69",
+    "url": "/rfkinline/IOT-React-coin-exchange/static/css/2.0b53e794.chunk.css"
+  },
+  {
+    "revision": "62cbd1efb454dd4f220d",
+    "url": "/rfkinline/IOT-React-coin-exchange/static/css/main.db1bca4b.chunk.css"
+  },
+  {
+    "revision": "71db51d90cafd0153c69",
+    "url": "/rfkinline/IOT-React-coin-exchange/static/js/2.43741ee3.chunk.js"
+  },
+  {
+    "revision": "1b9f3bf752a877bd1a04dbbaede1ef59",
+    "url": "/rfkinline/IOT-React-coin-exchange/static/js/2.43741ee3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62cbd1efb454dd4f220d",
+    "url": "/rfkinline/IOT-React-coin-exchange/static/js/main.91c88743.chunk.js"
+  },
+  {
+    "revision": "d70157ab5948a40a62f1",
+    "url": "/rfkinline/IOT-React-coin-exchange/static/js/runtime-main.afcf7157.js"
+  }
+]);
